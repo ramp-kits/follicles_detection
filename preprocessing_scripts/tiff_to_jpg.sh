199031016a2f0406b49beb04b9ef2d39d043634b
benjaminhabert <benj.habert@gmail.com>
2021-10-25 15:18:58 +0200

    possible solution to jpg conversion with small memory footprint

--- preprocessing_scripts/tiff_to_jpg.sh
+++ preprocessing_scripts/tiff_to_jpg.sh
@@ -1,3 +1,9 @@
+# usage:
+# ./preprocessing_scripts/tiff_to_jpg.sh
+# (required imagemagick)
+# Notes:
+# https://www.imagemagick.org/script/architecture.php#tera-pixel
+set -e
 INPUT_FOLDER="data/coupes_tiff"
 OUTPUT_FOLDER="data/coupes_jpg"
 echo "Converting images from $INPUT_FOLDER to $OUTPUT_FOLDER"
@@ -6,7 +12,11 @@ mkdir -p $OUTPUT_FOLDER
 for f in $INPUT_FOLDER/*.tiff
 do  
     echo "Converting $f" 
-    convert $f -set filename: "%t" $OUTPUT_FOLDER/%[filename:].jpg
+    convert\
+        -define registry:temporary-path=./data/tmp/ \
+        -limit memory 500mb\
+        $f\
+        -set filename: "%t" $OUTPUT_FOLDER/%[filename:].jpg
 done
 
 echo "Done"
