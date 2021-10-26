b7ba6f4dcfe93e17f77fe7614877e0144a892d91
benjaminhabert <benj.habert@gmail.com>
2021-10-26 11:53:15 +0200

    remove unecessary imagemagick config

--- preprocessing_scripts/tiff_to_jpg.sh
+++ preprocessing_scripts/tiff_to_jpg.sh
@@ -1,8 +1,9 @@
 # usage:
 # ./preprocessing_scripts/tiff_to_jpg.sh
 # (required imagemagick)
-# Notes:
+# Notes on converting large images:
 # https://www.imagemagick.org/script/architecture.php#tera-pixel
+# https://github.com/ImageMagick/ImageMagick/issues/396
 set -e
 INPUT_FOLDER="data/coupes_tiff"
 OUTPUT_FOLDER="data/coupes_jpg"
@@ -13,8 +14,6 @@ for f in $INPUT_FOLDER/*.tiff
 do  
     echo "Converting $f" 
     convert\
-        -define registry:temporary-path=./data/tmp/ \
-        -limit memory 500mb\
         $f\
         -set filename: "%t" $OUTPUT_FOLDER/%[filename:].jpg
 done
