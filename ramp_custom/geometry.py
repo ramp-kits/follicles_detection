1398c62ee3e0a0ab6c3f99de3a7e2296c73abaa9
benjaminhabert <benj.habert@gmail.com>
2021-10-29 16:30:09 +0200

    add docstrings

--- ramp_custom/geometry.py
+++ ramp_custom/geometry.py
@@ -1,20 +1,28 @@
 import numpy as np
 
 
-def find_matching_bbox(prediction, list_of_true_values, iou_thresholdd):
-    """
+def find_matching_bbox(prediction, list_of_true_values, iou_threshold):
+    """Find the index of the bounding box that is closest to the prediction.
 
     Parameters
     ----------
-    prediction: dict
-        with keys "image", "bbox"
-    list_of_true_values: list of dict
-        same keys
+    prediction : dict
+        with keys "image", "bbox". This is the reference bounding box against which
+        we are comparing.
+    list_of_true_values : list of dict
+        same keys as prediction. Only true_values belonging to the same image
+        as the prediction are taken into account.
 
 
-    Return index, success
-        index = index of bbox with highest iou
-        success = if matching iou is greater than threshold
+    Returns
+    -------
+    index : int
+        index in the ``list_of_true_values`` array of the bounding box that
+        is the closest to the ``prediction``.
+    success : bool
+        True if the IoU between the prediction and highest scoring true_value
+        is higher than the ``iou_threshold``
+
     """
     predicted_bbox = np.array(prediction["bbox"]).reshape(1, 4)
     all_true_bbox = np.array([value["bbox"] for value in list_of_true_values]).reshape(
@@ -28,22 +36,32 @@ def find_matching_bbox(prediction, list_of_true_values, iou_thresholdd):
     ious[is_different_image] = 0
 
     index, maximum = np.argmax(ious), np.max(ious)
-    return index, maximum > iou_thresholdd
+    return index, maximum > iou_threshold
 
 
 def compute_iou(boxes1, boxes2):
     """Computes pairwise IOU matrix for given two sets of boxes
 
-    Arguments:
-      boxes1: A tensor with shape `(N, 4)` representing bounding boxes
-        where each box is of the format `[x, y, x2, y2]`.
-        boxes2: A tensor with shape `(M, 4)` representing bounding boxes
-        where each box is of the format `[x, y, xmax, ymax]`.
+    IOU : Intersection over Union.
+    0 < IOU < 1
 
-    Returns:
-      pairwise IOU matrix with shape `(N, M)`, where the value at ith row
-        jth column holds the IOU between ith box and jth box from
+    Doc: https://github.com/rafaelpadilla/Object-Detection-Metrics
+
+    Parameters
+    ----------
+    boxes1 : np.array of shape `(N, 4)`
+        these representi bounding boxes
+        where each box is of the format `[x, y, x2, y2]`.
+    boxes2 : np.array shape `(M, 4)`
+        same format as ``boxes1``
+
+    Returns
+    -------
+    iou_matrix : np.array of shape `(N, M)`
+        pairwise IOU matrix with shape `(N, M)`, where the value at i_th row
+        j_th column holds the IOU between ith box and jth box from
         boxes1 and boxes2 respectively.
+
     """
     lu = np.maximum(boxes1[:, None, :2], boxes2[:, :2])
     rd = np.minimum(boxes1[:, None, 2:], boxes2[:, 2:])
@@ -58,6 +76,22 @@ def compute_iou(boxes1, boxes2):
 
 
 def apply_NMS_for_y_pred(y_pred, iou_threshold):
+    """Remove duplicate predictions using Non Negative Suppression.
+
+    Parameters
+    ----------
+    y_pred : np.array of shape (N_images,)
+        each element is a list of predicted locations.
+    iou_threshold : float
+        if multiple locations (of the same class and for the same image)
+        have an IoU higher than this threshold, only the one with the highest
+        probability is kept.
+
+    Returns
+    -------
+    y_pred_filtered : np.array of shape (N_images,)
+
+    """
     filtered_predictions = [
         apply_NMS_for_image(predictions, iou_threshold) for predictions in y_pred
     ]
@@ -67,6 +101,12 @@ def apply_NMS_for_y_pred(y_pred, iou_threshold):
 
 
 def apply_NMS_for_image(predictions, iou_threshold):
+    """Remove duplicate predictions using Non Negative Suppression.
+
+    Similar to ``apply_NMS_for_y_pred`` but expects a list of
+    predictions as input.
+
+    """
     classes = set(pred["class"] for pred in predictions)
     filtered_predictions = []
     for class_name in classes:
